1bd458d6750894a3fe8e11e8f876ba0d4e6a94f0
Gabriel Nilsson Högberg <gabrielnilsson.hogb@elev.ga.ntig.se>
2024-04-11 11:08:45 +0200

    ✨ Signup, login, nya routes

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,75 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
 
 const pool = require('../db')
+
 
 router.get('/', function (req, res) {
     res.render('index.njk', { title: 'Game reviews' })
 })
 
-router.get('/login', function (req, res) {
-    res.render('login.njk', {
+router.post('/login', async function (req, res) {
+    const username = req.body.username
+    
+    const [user] = await pool.promise().query(
+        'SELECT id, password FROM gabriel_login WHERE username = ?', [username]
+        )
+        
+        req.session.userid = user[0].id
+        const passwordenter = req.body.password
+        
+        bcrypt.compare(passwordenter, user[0].password, function (err, result) {
+            req.session.username = username
+        if (result) {
+            req.session.login = true
+            res.redirect('/minasidor')
+            console.log(req.session.login)
+        } else {
+            res.json({ message: 'Fel lösenord' })
+        }
+    });
+})
 
+router.get('/login', function (req, res) {
+    res.render('login.njk')
+})
+
+router.post('/signup', async function (req, res) {
+    const username = req.body.username
+    const password = req.body.password
+    
+    bcrypt.hash(password, 10, async function (err, hash) {
+        try {
+            const [user] = await pool.promise().query('INSERT INTO `gabriel_login` ( `username`, `password`) VALUES( ?, ?)', [username, hash])
+            res.redirect('/login')
+        } catch (error) {
+            console.log(error)
+            res.status(402)
+        }
     })
 })
 
 router.get('/signup', function (req, res) {
-    res.render('signup.njk', {
+    res.render('signup.njk')
+})
 
+router.get('minasidor', function (req, res) {
+    console.log(req.session)
+    if (req.session.login) {
+        res.render('minasidor.njk', {
+            username: req.session.username,
+            message: 'Välkommen'
+        })
+    } else {
+        res.redirect('/login')
+    }
+})
+
+router.get('/hashTest', async function (req, res) {
+    bcrypt.hash('hashtest123', 10, function (err, hash) {
+        console.log(hash)
+        return res.json(hash);
     })
 })
 
