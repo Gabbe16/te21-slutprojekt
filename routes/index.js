9f389edc1bb10491a8633b2fded1063f464fc303
Gabriel Nilsson Högberg <gabrielnilsson.hogb@elev.ga.ntig.se>
2024-04-15 13:17:13 +0200

    ✨ Login, signup, logout och minasidor

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,50 @@
 const express = require('express')
-const router = express.Router()
 const bcrypt = require('bcrypt')
+const router = express.Router()
 
 const pool = require('../db')
 
-
 router.get('/', function (req, res) {
-    res.render('index.njk', { title: 'Game reviews' })
+    res.render('index.njk', {
+        title: 'Game reviews',
+        username: req.session.username
+    })
 })
 
 router.post('/login', async function (req, res) {
     const username = req.body.username
-    
+
     const [user] = await pool.promise().query(
         'SELECT id, password FROM gabriel_login WHERE username = ?', [username]
-        )
-        
-        req.session.userid = user[0].id
-        const passwordenter = req.body.password
-        
-        bcrypt.compare(passwordenter, user[0].password, function (err, result) {
-            req.session.username = username
+    )
+
+    const passwordenter = req.body.password
+
+    bcrypt.compare(passwordenter, user[0].password, function (err, result) {
+        console.log(result)
         if (result) {
+            req.session.userid = user[0].id
+            req.session.username = username
             req.session.login = true
             res.redirect('/minasidor')
-            console.log(req.session.login)
+            console.log(req.session)
         } else {
+            console.log(err)
             res.json({ message: 'Fel lösenord' })
         }
     });
 })
 
 router.get('/login', function (req, res) {
-    res.render('login.njk')
+    res.render('login.njk', {
+        username: req.session.username
+    })
 })
 
 router.post('/signup', async function (req, res) {
     const username = req.body.username
     const password = req.body.password
-    
+
     bcrypt.hash(password, 10, async function (err, hash) {
         try {
             const [user] = await pool.promise().query('INSERT INTO `gabriel_login` ( `username`, `password`) VALUES( ?, ?)', [username, hash])
@@ -51,10 +57,12 @@ router.post('/signup', async function (req, res) {
 })
 
 router.get('/signup', function (req, res) {
-    res.render('signup.njk')
+    res.render('signup.njk', {
+        username: req.session.username
+    })
 })
 
-router.get('minasidor', function (req, res) {
+router.get('/minasidor', function (req, res) {
     console.log(req.session)
     if (req.session.login) {
         res.render('minasidor.njk', {
@@ -66,6 +74,18 @@ router.get('minasidor', function (req, res) {
     }
 })
 
+router.get('/logout', function (req, res) {
+    req.session.destroy(function (err) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.redirect('/')
+        }
+    })
+})
+
+/*
+
 router.get('/hashTest', async function (req, res) {
     bcrypt.hash('hashtest123', 10, function (err, hash) {
         console.log(hash)
@@ -73,7 +93,6 @@ router.get('/hashTest', async function (req, res) {
     })
 })
 
-
-
+*/
 
 module.exports = router
